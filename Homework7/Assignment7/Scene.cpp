cbb5e7fa8fb37af085beef521d4bb94f61b659d8
thomation <thomation@126.com>
2021-08-22 09:51:54 +0800

    compute direct light

--- Homework7/Assignment7/Scene.cpp
+++ Homework7/Assignment7/Scene.cpp
@@ -3,7 +3,7 @@
 //
 
 #include "Scene.hpp"
-
+#include <cmath>
 
 void Scene::buildBVH() {
 	printf(" - Generating BVH...\n\n");
@@ -72,29 +72,38 @@ Vector3f Scene::castRay(const Ray& ray, int depth) const
 	Vector2f uv;
 	uint32_t index = 0;
 	if (intersection.happened) {
+		// Light
+		//if (intersection.obj->hasEmit()) {
+			//hitColor = m->getEmission()/ 255.0f;
+			//return hitColor;
+		//}
 		const Vector3f& shadePoint = intersection.coords;
+		Vector3f N = intersection.normal; // normal
+		Vector2f st; // st coordinates
+		hitObject->getSurfaceProperties(shadePoint, ray.direction, index, uv, N, st);
+		const Vector3f wo = - ray.direction;
+		Vector3f L_dir = Vector3f(0);
 		{
 			Intersection lightSample;
 			float lightPdf;
 			sampleLight(lightSample, lightPdf);
 			Vector3f lightPos = lightSample.coords;
-			Vector3f rayDir = normalize(lightPos - shadePoint);
-			Ray ray = Ray(shadePoint + rayDir, rayDir);
+			Vector3f ws = normalize(lightPos - shadePoint);
+			Ray ray = Ray(shadePoint + ws, ws);
 			Intersection lightIntersction = Scene::intersect(ray);
-			if (lightIntersction.happened && (lightIntersction.coords - lightPos).norm() < EPSILON)
+			if (lightIntersction.happened && lightIntersction.obj->hasEmit() && (lightIntersction.coords - lightPos).norm()< EPSILON)
 			{
-				hitColor = Vector3f(1, 0, 0);
-			} else {
-				hitColor = Vector3f(0, 1, 0);
+				auto emit = lightSample.emit / 255.0f;
+				auto d2 = (lightPos - shadePoint).norm();
+				auto f_r = m->eval(wo, ws, N);
+				auto cp = std::max(0.0f, dotProduct(ws, N));
+				Vector3f NN = lightIntersction.normal;
+				lightIntersction.obj->getSurfaceProperties(lightPos, ws, index, uv, NN, st);
+				auto cl = std::max(0.0f, dotProduct(-ws, NN));
+				L_dir = emit * f_r * cp * cl /( d2 * lightPdf + EPSILON);
 			}
-
 		}
-		Vector3f N = intersection.normal; // normal
-		Vector2f st; // st coordinates
-		hitObject->getSurfaceProperties(shadePoint, ray.direction, index, uv, N, st);
-		// Light
-		if (intersection.obj->hasEmit())
-			hitColor = Vector3f(1, 1, 1);
+		hitColor = L_dir;
 	}
 	return hitColor;
 }

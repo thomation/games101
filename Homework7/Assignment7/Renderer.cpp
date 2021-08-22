a09ad09e831210a001ccd828cc3bdbd47ce2353a
thomation <thomation@126.com>
2021-08-22 20:06:07 +0800

    omp support

--- Homework7/Assignment7/Renderer.cpp
+++ Homework7/Assignment7/Renderer.cpp
@@ -24,10 +24,12 @@ void Renderer::Render(const Scene& scene)
     int m = 0;
 
     // change the spp value to change sample ammount
-    int spp = 16;
-    std::cout << "SPP: " << spp << "\n";
-    for (uint32_t j = 0; j < scene.height; ++j) {
-        for (uint32_t i = 0; i < scene.width; ++i) {
+    int spp = 128;
+    std::cout << "SPP: " << spp << "\n"; 
+
+#pragma omp parallel for
+    for (int j = 0; j < scene.height; ++j) {
+        for (int i = 0; i < scene.width; ++i) {
             // generate primary ray direction
             float x = (2 * (i + 0.5) / (float)scene.width - 1) *
                       imageAspectRatio * scale;
@@ -35,9 +37,8 @@ void Renderer::Render(const Scene& scene)
 
             Vector3f dir = normalize(Vector3f(-x, y, 1));
             for (int k = 0; k < spp; k++){
-                framebuffer[m] += scene.castRay(Ray(eye_pos, dir), 0) / spp;  
+                framebuffer[j * scene.width + i] += scene.castRay(Ray(eye_pos, dir), 0) / spp;  
             }
-            m++;
         }
         UpdateProgress(j / (float)scene.height);
     }

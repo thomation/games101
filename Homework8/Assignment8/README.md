95ae26f9222d58903aebb6a4b2fdf24b18c73c95
thomation <thomation@126.com>
2021-08-28 16:25:12 +0800

    generate and show rope

---
+++ Homework8/Assignment8/README.md
@@ -0,0 +1,3 @@
+# GAMES101 homework8 完成情况
+* 连接绳子约束,构造绳子
+* 在win10的wsl ubuntu虚拟机xface4下运行。 
--- Homework8/Assignment8/src/rope.cpp
+++ Homework8/Assignment8/src/rope.cpp
@@ -12,11 +12,22 @@ namespace CGL {
     Rope::Rope(Vector2D start, Vector2D end, int num_nodes, float node_mass, float k, vector<int> pinned_nodes)
     {
         // TODO (Part 1): Create a rope starting at `start`, ending at `end`, and containing `num_nodes` nodes.
-
 //        Comment-in this part when you implement the constructor
-//        for (auto &i : pinned_nodes) {
-//            masses[i]->pinned = true;
-//        }
+        if(num_nodes < 2)
+        {
+            std::cerr << "nodes < 2" << std::endl;
+            return;
+        }
+        auto step = (end - start) / (num_nodes - 1);
+        for(int i = 0; i < num_nodes; i ++)
+            masses.push_back(new Mass(start + step * i, node_mass, false));
+        for (auto &i : pinned_nodes) {
+            masses[i]->pinned = true;
+        }
+        for(int i = 0; i < num_nodes - 1; i ++)
+        {
+            springs.push_back(new Spring(masses[i], masses[i + 1], k));
+        }
     }
 
     void Rope::simulateEuler(float delta_t, Vector2D gravity)
